b59bfb5a7779814e75e1bb3cc654271eeb9e74d0
CourtVision1 <96318326+CourtVision1@users.noreply.github.com>
2022-10-14 14:45:04 -0400

    Won't build (#4)

---
+++ OCKSample/Main/Stylers/AppearanceStyle.swift
@@ -0,0 +1,18 @@
+//
+//  AppearanceStyle().swift
+//  OCKSample
+//
+//  Created by  on 10/14/22.
+//  Copyright © 2022 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
+import CareKitUI
+
+struct AppearanceStyle: OCKAppearanceStyler {
+    var cornerRadius1: CGFloat { 10 }
+    var cornerRadius2: CGFloat { 8 }
+
+    var borderWidth1: CGFloat { 2 }
+    var borderWidth2: CGFloat { 1 }
+}
