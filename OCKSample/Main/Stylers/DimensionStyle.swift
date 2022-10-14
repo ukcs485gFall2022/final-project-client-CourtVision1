62f5eed39c1322174ab3118a0d614741fec65309
CourtVision1 <alexcox122@gmail.com>
2022-10-14 16:25:06 -0400

    Fixed issue with code not building

--- OCKSample/Main/Stylers/DimensionStyle.swift
+++ OCKSample/Main/Stylers/DimensionStyle.swift
@@ -10,9 +10,9 @@ import Foundation
 import CareKitUI
 
 struct DimensionStyle: OCKDimensionStyler{
-    var imageHeight2: CGFloat { 55 }
-    var imageHeight1: CGFloat { 160 }
-    var pointSize3: CGFloat { 12 }
-    var pointSize2: CGFloat { 15 }
-    var pointSize1: CGFloat { 18 }
+    var imageHeight2: CGFloat { 10 }
+    var imageHeight1: CGFloat { 10 }
+    var pointSize3: CGFloat { 20 }
+    var pointSize2: CGFloat { 30 }
+    var pointSize1: CGFloat { 40 }
 }
